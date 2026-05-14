3b8ce6617a5445524c5a3ce1776beec397b88cff
小泉健太郎 <ke-koizumi@sts-inc.co.jp>
2026-05-14 23:31:00 +0900

    フォーマット

--- src/.config/nvim/lua/plugins/snacks.lua
+++ src/.config/nvim/lua/plugins/snacks.lua
@@ -1,51 +1,51 @@
 return {
-        "folke/snacks.nvim",
-        priority = 1000,
-        lazy = false,
-        opts = {
-                picker = { enabled = true },
-                notifier = { enabled = true },
-        },
-        keys = {
-                {
-                        "<Leader>ff",
-                        function()
-                                Snacks.picker.files()
-                        end,
-                        mode = "n",
-                        desc = "検索: ファイルを探して開く",
-                },
-                {
-                        "<Leader>fg",
-                        function()
-                                Snacks.picker.grep()
-                        end,
-                        mode = "n",
-                        desc = "検索: プロジェクト内を文字列検索",
-                },
-                {
-                        "<Leader>fb",
-                        function()
-                                Snacks.picker.buffers()
-                        end,
-                        mode = "n",
-                        desc = "検索: バッファを切り替える",
-                },
-                {
-                        "<Leader>fh",
-                        function()
-                                Snacks.picker.help()
-                        end,
-                        mode = "n",
-                        desc = "検索: ヘルプタグを探す",
-                },
-                {
-                        "<Leader>gg",
-                        function()
-                                Snacks.lazygit()
-                        end,
-                        mode = "n",
-                        desc = "Git: lazygit を開く",
-                },
-        },
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    picker = { enabled = true },
+    notifier = { enabled = true },
+  },
+  keys = {
+    {
+      "<Leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      mode = "n",
+      desc = "検索: ファイルを探して開く",
+    },
+    {
+      "<Leader>fg",
+      function()
+        Snacks.picker.grep()
+      end,
+      mode = "n",
+      desc = "検索: プロジェクト内を文字列検索",
+    },
+    {
+      "<Leader>fb",
+      function()
+        Snacks.picker.buffers()
+      end,
+      mode = "n",
+      desc = "検索: バッファを切り替える",
+    },
+    {
+      "<Leader>fh",
+      function()
+        Snacks.picker.help()
+      end,
+      mode = "n",
+      desc = "検索: ヘルプタグを探す",
+    },
+    {
+      "<Leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      mode = "n",
+      desc = "Git: lazygit を開く",
+    },
+  },
 }
